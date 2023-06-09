b264ecb25b1e70208b26861e547e123d76fa0401
Tim Maupin <timmaupin@live.com>
2023-06-09 12:35:33 -0500

    refactoring and tweaking code for test

--- __test__/auth.test.js
+++ __test__/auth.test.js
@@ -5,30 +5,41 @@ const { app } = require('../src/server');
 const { sequelize } = require('../src/auth/models');
 const { describe, test } = require('node:test');
 
-const request = super(app);
+const request = supertest(app);
 
 beforeAll( async () => {
-    await sequelize.sync();
+  await sequelize.sync();
 });
 
 afterAll(async () => {
-    await sequelize.drop();
+  await sequelize.drop();
 });
 
 describe('Auth Routes', (() => {
-    test('allow for users signup', async() => {
-        const response = await request.post('/signup').sen({
-            username: 'Bob'
-            password: 'joy'
-        });
-
-        expect(response.status).toEqual(200);
-        expect(response.body.username).toEqual('Bob');
+  test('allow for users signup', async() => {
+    const response = await request.post('/signup').sen({
+      username: 'Bob',
+      password: 'joy',
     });
 
-    test('allow for uer signin', async () => {
-        const reponse = await request.post('/signin').set('Authorization', 'Basic');
+    expect(response.status).toEqual(200);
+    expect(response.body.username).toEqual('Bob');
+  });
 
-        expect(response.status).toEqual(200);
-    });
+  test('allow for user sign-in', async () => {
+    const response = await request.post('/sign-in').set('Authorization', 'Basic');
+
+    expect(response.status).toEqual(200);
+    expect(response.body.username).toEqual('Testing approved');
+  });
+
+  test('/sign-in failed', async () => {
+    let request = { headers: { authorization: 'Bad Basic'} };
+    let response = { status: jest.fn() };
+    let next = jest.fn();
+    await basicAuth(request, response, next);
+
+    expect(next).toHaveBeenCalledWith('Login Failed');
+  });
+  
 }));
--- src/auth/middleware/basic/index.js
+++ src/auth/middleware/basic/index.js
@@ -2,27 +2,27 @@
 
 const bcrypt = require('bcrypt');
 const base64 = require('base-64');
-const { Users } = require('../../models')
+const { Users } = require('../../models');
 
 
 module.export = async (req, res, next) => {
 
-        let basicHeaderParts = req.headers.authorization.split(' ');
-        let encodedString = basicHeaderParts.pop();
-        let decodedString = base64.decode(encodedString);
-        let [username, password] = decodedString.split(':');
+  let basicHeaderParts = req.headers.authorization.split(' ');
+  let encodedString = basicHeaderParts.pop();
+  let decodedString = base64.decode(encodedString);
+  let [username, password] = decodedString.split(':');
 
-        try {
-          const user = await Users.findOne({ where: { username: username } });
-          const valid = await bcrypt.compare(password, user.password);
-          if (valid) {
-            req.user = user;
-            next();
-            res.status(200).json(user);
-          }
-          else {
-            throw new Error('Invalid User');
-          }
-        } catch (error) { next('Invalid Login. message: ', error.message); }
+  try {
+    const user = await Users.findOne({ where: { username: username } });
+    const valid = await bcrypt.compare(password, user.password);
+    if (valid) {
+      req.user = user;
+      next();
+      res.status(200).json(user);
+    }
+    else {
+      throw new Error('Invalid User');
+    }
+  } catch (error) { next('Invalid Login. message: ', error.message); }
 
-    };
+};
