8c5eb2674efa6851767760cd3258cd65d3094533
Tim Maupin <timmaupin@live.com>
2023-06-09 13:32:52 -0500

    cleaned up router.js and got rid of a some extra code

--- src/auth/router.js
+++ src/auth/router.js
@@ -5,44 +5,28 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 const { Users } = require('./models');
 const base64 = require('base-64');
-const basicAuth = require('./middleware/')
+const basicAuth = require('./middleware/');
 
 router.post('/signup', async (request, response, next) => {
-
-    app.post('/signup', async (req, res) => {
-
-        try {
-            req.body.password = await bcrypt.hash(req.body.password, 10);
-            const record = await Users.create(req.body);
-            res.status(200).json(record);
-        } catch (e) { res.status(403).send('Error Creating User'); }
-    });
-},
-
-
-  app.post('/signin', async (req, res) => {
+  try {
+    request.body.password = await bcrypt.hash(req.body.password, 10);
+    const record = await Users.create(req.body);
+    response.status(200).json(record);
+  } catch (error) {
+    response.status(403).send('Error Creating User');
+  }
+});
 
 
 
-    let basicHeaderParts = req.headers.authorization.split(' ');  // ['Basic', 'am9objpmb28=']
-    let encodedString = basicHeaderParts.pop();  // am9objpmb28=
-    let decodedString = base64.decode(encodedString); // "username:password"
-    let [username, password] = decodedString.split(':'); // username, password
+router.post('/signin', basicAuth, async (request, response, next) => {
 
+  try {
+    response.status(200).json(user);
 
-    try {
-        const user = await Users.findOne({ where: { username: username } });
-        const valid = await bcrypt.compare(password, user.password);
-        if (valid) {
-            req.user = user;
-            next();
-            res.status(200).json(user);
-        }
-        else {
-            throw new Error('Invalid User');
-        }
-    } catch (error) { next('Invalid Login. message: ', error.message); }
+  } catch (error) {
+    next('Invalid Login. message: ', error.message); }
 
-}),
+});
 
 module.exports = router;
